5b12c5cf7f8105e7dbc38a755145a9fab5a7d6f2
dluck <dutiesheng@outlook.com>
2018-06-25 09:32:54 +0800

    美化前端页面

--- src/main/java/com/dluck/wordscount/controller/WordController.java
+++ src/main/java/com/dluck/wordscount/controller/WordController.java
@@ -4,7 +4,6 @@ import com.dluck.wordscount.Service.FileService;
 import com.dluck.wordscount.Service.WordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
